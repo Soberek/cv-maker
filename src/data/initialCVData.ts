4f2d1ed78f0731cc0fb4ab53f5a189070f48ac22
Sober <59960384+Soberek@users.noreply.github.com>
2023-08-03 23:28:53 +0200

    feat: remodeled initial cv data with it's type definition

--- src/data/initialCVData.ts
+++ src/data/initialCVData.ts
@@ -3,34 +3,28 @@ import { initialCVDataI } from "../types/initialCVData";
 export const initialCVData: initialCVDataI = {
   personal_info: {
     name: "Krzysztof Palpuchowski",
-    job_title: "Frontend Developer",
+    job_title: "Front-end Developer",
     description:
-      "Frontend developer with a passion for crafting user-friendly web applications. Skilled in HTML, CSS, and JavaScript, I create seamless and interactive interfaces to deliver high-quality user experiences.",
-  },
-  links: {
+      "Frontend developer with 2+ years of experience in frontend development. I am passionate about frontend development and I love to learn new technologies. I am also passionate about learning new languages and technologies",
     email: "palpuchowski.k@gmail.com",
     phone: "+48 791 879 326",
-    github_link: "github.com",
-    linkedin_link: "linkedin.com",
+    github: "https://github.com/Soberek",
   },
   skills: [
-    "TypeScript",
-    "React",
-    "HTML",
-    "CSS",
-    "RWD",
-    "Test Driven Development (TDD)",
-    "Git",
-    "Github",
-    "Vite",
-    "Vitest",
-    "Jest",
-    "APIs",
-    "Redux",
-    "TailwindCSS",
-    "MaterialUI",
-    "react-query",
+    { skill_name: "SCSS" },
+    { skill_name: "TypeScript" },
+    { skill_name: "React" },
+    { skill_name: "Redux" },
+    { skill_name: "RWD" },
+    { skill_name: "Git" },
+    { skill_name: "Vitest" },
+    { skill_name: "TailwindCSS" },
+    { skill_name: "MaterialUI" },
+    { skill_name: "React Query" },
+    { skill_name: "APIs" },
+    { skill_name: "Figma" },
   ],
+
   work_experience: [
     {
       company: "",
@@ -41,19 +35,35 @@ export const initialCVData: initialCVDataI = {
   ],
   education: [
     {
-      institution: "",
-      degree: "",
-      startDate: "",
-      endDate: "",
+      school_name: "",
+      school_location: "",
+      major: "",
+      start_date: "",
+      end_date: "",
     },
   ],
   projects: [
     {
-      name: "Project 1",
-      source: "Source 1",
-      demo: "Demo 1",
-      description: [""],
+      name: "CVMaker",
+      tools: ["React", "Redux", "React Query", "MaterialUI", "Figma", "Git"],
+      source: "https://github.com/Soberek/cv-maker",
+      demo: "https://netlify.com/soberek/project1",
+      description: [
+        "Utilized JavaScript for front-end development, creating a user-friendly and interactive interface,",
+        "Integrated a live preview functionality, allowing users to see their CV in real-time as they make changes,",
+        "Incorporated data validation to ensure the accuracy and completeness of CV entries,",
+        "Performed rigorous testing of the application using Vitest framework to ensure high-quality code and functionality.",
+      ],
     },
   ],
-  interests: ["Interest 1", "Interest 2"],
+  languages: [
+    {
+      name: "Polish",
+      proficiency: "Native",
+    },
+    {
+      name: "English",
+      proficiency: "B2/C1",
+    },
+  ],
 };
--- src/types/initialCVData.d.ts
+++ src/types/initialCVData.d.ts
@@ -1,40 +1,49 @@
-export interface initialCVDataI {
-  personal_info: {
-    name: string;
-    job_title: string;
-    description: string;
-  };
-  links: {
-    email: string;
-    github_link: string;
-    linkedin_link: string;
-    phone: string;
-  };
-  skills: string[];
-  work_experience?: [
-    {
-      company: string;
-      position: string;
-      startDate: string;
-      endDate: string;
-    },
-  ];
+export interface PersonalInfoI {
+  name: string;
+  job_title: string;
+  description: string;
+  email: string;
+  github: string;
+  phone: string;
+}
 
-  education: [
-    {
-      institution: string;
-      degree: string;
-      startDate: string;
-      endDate: string;
-    },
-  ];
-  interests: string[];
-  projects: [
-    {
-      name: string;
-      source: string;
-      demo: string;
-      description: string[];
-    },
-  ];
+export interface ProjectI {
+  name: string;
+  tools: string[];
+  source: string;
+  demo: string;
+  description: string[];
+}
+
+interface SkillI {
+  skill_name: string;
+}
+
+export interface LanguageI {
+  name: string;
+  proficiency: string;
+}
+
+export interface EducationI {
+  school_name: string;
+  school_location: string;
+  major: string;
+  start_date: string;
+  end_date: string;
+}
+
+export interface WorkExperienceI {
+  company: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface initialCVDataI {
+  personal_info: PersonalInfoI;
+  skills: SkillI[];
+  work_experience?: WorkExperienceI[];
+  education: EducationI[];
+  languages: LanguageI[];
+  projects: ProjectI[];
 }
