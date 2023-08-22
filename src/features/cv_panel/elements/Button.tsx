a3eb8b306850a46a01b6013ba4f43e33301f8609
Sober <59960384+Soberek@users.noreply.github.com>
2023-08-22 16:04:25 +0200

    feat: Introduced different sizes for Button component

--- src/features/cv_panel/elements/Button.tsx
+++ src/features/cv_panel/elements/Button.tsx
@@ -2,11 +2,29 @@ type ButtonProps = {
   onClick: () => void;
   children: string;
   type?: "submit" | "button";
+  size?: "small" | "medium" | "large";
 };
 
-const Button = ({ onClick, children, type }: ButtonProps) => {
+const Button = ({ onClick, children, type, size }: ButtonProps) => {
+  let buttonSize = "px-1 py-1";
+  switch (size) {
+    case "small":
+      buttonSize = "px-1 py-1";
+      break;
+    case "medium":
+      buttonSize = "px-2 py-2";
+      break;
+    case "large":
+      buttonSize = "px-4 py-4";
+      break;
+  }
+
   return (
-    <button onClick={onClick} className="border-2 border-cyan-500 px-2 py-2 text-cyan-500" type={type || "button"}>
+    <button
+      onClick={onClick}
+      className={`border-2 border-cyan-500 text-cyan-500  ${buttonSize}`}
+      type={type || "button"}
+    >
       {children}
     </button>
   );
